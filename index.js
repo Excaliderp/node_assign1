531ddbfb9424969b8db49499f74e7c97ab30a437
Timothy Karlsson <timothy.karlsson@edu.cmeducations.se>
2023-05-08 13:53:31 +0200

    can get api

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from "express";
+import {mockData} from "./mockData.js";
 
 const app = express()
-const port = 3005
+const port = 3006
+let movies = mockData
 
 app.get('/', (req, res) => {
   res.send('Hola amigo!')
-})
+});
+
+app.get("/movies", (req, res) => {
+    res.json(movies)
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
---
+++ routes/movies.js
@@ -0,0 +1,12 @@
+// import express from "express";
+// import router from express.Router();
+// import mockData from "../mockData"
+
+// let movies = mockData
+
+
+// router.get("/", (req, res) => {
+//     res.json(movies)
+// });
+
+// module.exports = router;
