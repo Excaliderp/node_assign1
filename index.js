1f50311152e33767c2e0d57d3f4a877e0f9dcd92
Excaliderp <timothy.karlsson@edu.cmeducations.se>
2023-05-09 15:57:12 +0200

    ad search movie id by array index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {mockData} from "./mockData.js";
 const app = express()
 const port = 3006
 let movies = mockData
+const id = mockData.indexOf
 
 app.get('/', (req, res) => {
   res.send('Hola amigo!')
@@ -11,6 +12,18 @@ app.get('/', (req, res) => {
 
 app.get("/movies", (req, res) => {
     res.json(movies)
+});
+
+app.get("/movies/:id", (req, res) => {
+  const index = parseInt(req.params.id);
+  // const index = movies.findIndex((film) => film.id === id);
+  if (index !== -1) {
+    const movie = movies[index];
+    res.json(movie);
+  } else {
+    res.status(404).json({ message: "Movie not found" });
+  }
+
 });
 
 app.listen(port, () => {
