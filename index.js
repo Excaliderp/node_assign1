e020cb0fcf21bbd138b4c645da139ad5749d2ec0
Excaliderp <timothy.karlsson@edu.cmeducations.se>
2023-05-09 16:25:56 +0200

    fixed so sticking to array index logic is not necessary

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 import express from "express";
-import {mockData} from "./mockData.js";
+import { mockData } from "./mockData.js";
 
-const app = express()
-const port = 3006
-let movies = mockData
-const id = mockData.indexOf
+const app = express();
+const port = 3006;
+let movies = mockData;
+const id = mockData.indexOf;
 
-app.get('/', (req, res) => {
-  res.send('Hola amigo!')
+app.get("/", (req, res) => {
+  res.send("Hola amigo!");
 });
 
 app.get("/movies", (req, res) => {
-    res.json(movies)
+  res.json(movies);
 });
 
 app.get("/movies/:id", (req, res) => {
-  const index = parseInt(req.params.id);
+  const index = parseInt(req.params.id) - 1;
+  const movie = movies[index];
+
   // const index = movies.findIndex((film) => film.id === id);
-  if (index !== -1) {
-    const movie = movies[index];
-    res.json(movie);
-  } else {
+
+  if (!movie) {
     res.status(404).json({ message: "Movie not found" });
   }
 
+  res.json(movie);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
